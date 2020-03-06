b6a1b67ba3902372b31fc1b3f9ab274ba209113e
xaylpq <xaylpq@gmail.com>
2020-03-06 17:55:04 +0300

    version 1.0

---
+++ README.md
@@ -0,0 +1,4 @@
+<div align="center">
+  <h2>Простое адаптивное модальное окно на чистом Java Script</h2>
+  <img src="prev.png">
+</div>
---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+ 
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        document.body.style.overflow = '';
+    });
+
+});
